8279d3fc7242ff5567fff7ba8bfb4f69dc8f4d68
MiXeR54 <max_kray@list.ru>
2020-07-21 17:44:13 +0300

    timeStamp edit

--- src/components/Dialog/Chat.tsx
+++ src/components/Dialog/Chat.tsx
@@ -106,7 +106,7 @@ export const Chat = () => {
           sendMessage({
             income: !isSeller,
             text: message,
-            time: `${timeStamp.getHours()} : ${timeStamp.getMinutes()} : ${timeStamp.getSeconds()}`,
+            time: `${timeStamp}`,
           })
         );
         setMessage("");
